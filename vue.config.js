070837e9b17b8dd2d92546b47d4543a4a3777a92
fangjy <fangjingyu0502@163.com>
2021-01-21 18:43:04 +0800

    init commit

---
+++ vue.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  outputDir: process.env.VUE_APP_OUTPUT_DIR,
+  configureWebpack() {
+  },
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: '@import "@/assets/scss/var.scss";',
+      },
+    },
+  },
+  productionSourceMap: false,
+  // devServer: {
+  //   host: process.env.VUE_APP_HOST,
+  //   port: process.env.VUE_APP_PORT,
+  //   https: true,
+  //   proxy: {
+  //     '/api': {
+  //       target: process.env.VUE_APP_PROXY,
+  //       changeOrigin: true,
+  //     },
+  //   },
+  //   disableHostCheck: true,
+  // },
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      args[0].title = '青藏高原地区可视化';
+      return args;
+    });
+  },
+};
